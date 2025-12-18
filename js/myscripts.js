1214e426bca8229b5ee9d4b2c9c3e603e7dc67ec
viveksai26 <viveksai26@gmail.com>
2025-12-18 12:41:54 +0530

    Update myscripts.js

--- js/myscripts.js
+++ js/myscripts.js
@@ -3,58 +3,58 @@ if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("service-worker.js");
   }
 
-// let installPrompt = null;
-// const installButton = document.querySelector("#install");
+ let installPrompt = null;
+const installButton = document.querySelector("#install");
 
-// window.addEventListener("beforeinstallprompt", (event) => {
-//   console.log('beforeinstallprompt');
-//   // event.preventDefault();
-//   installPrompt = event;
-//   installButton.classList.remove("hide");
-// });
+window.addEventListener("beforeinstallprompt", (event) => {
+   console.log('beforeinstallprompt');
+    event.preventDefault();
+   installPrompt = event;
+   installButton.classList.remove("hide");
+ });
 
-// installButton.addEventListener("click", async () => {
-//   if (!installPrompt) {
-//     return;
-//   }
-//   const result = await installPrompt.prompt();
-//   console.log(`Install prompt was: ${result.outcome}`);
-//   if (result.outcome === 'accepted') {
-//     disableInAppInstallPrompt();
-//   }
-// });
+installButton.addEventListener("click", async () => {
+   if (!installPrompt) {
+     return;
+   }
+   const result = await installPrompt.prompt();
+   console.log(`Install prompt was: ${result.outcome}`);
+   if (result.outcome === 'accepted') {
+     disableInAppInstallPrompt();
+  }
+ });
 
-// window.addEventListener("appinstalled", () => {
-//   console.log('App already installed');
-//   disableInAppInstallPrompt();
-// });
+ window.addEventListener("appinstalled", () => {
+   console.log('App already installed');
+   disableInAppInstallPrompt();
+ });
 
-// function disableInAppInstallPrompt() {
-//   installPrompt = null;
-//   console.log('disable install prompt');
-//   installButton.classList.add("hide");
-// }
+ function disableInAppInstallPrompt() {
+   installPrompt = null;
+   console.log('disable install prompt');
+   installButton.classList.add("hide");
+ }
 
-// const button = document.getElementById("notifications");
-// button.addEventListener("click", () => {
-//   Notification.requestPermission().then((result) => {
-//     if (result === "granted") {
-//       randomNotification();
-//     } else {
-//       console.log('NOtifications rejected');
+ const button = document.getElementById("notifications");
+ button.addEventListener("click", () => {
+   Notification.requestPermission().then((result) => {
+     if (result === "granted") {
+       randomNotification();
+     } else {
+       console.log('NOtifications rejected');
       
-//     }
-//   });
-// });
+     }
+   });
+ });
 
-// function randomNotification() {
-//   const notifTitle = 'Title';
-//   const notifBody = `Body `;
-//   const notifImg = `./fprint.png`;
-//   const options = {
-//     body: notifBody,
-//     icon: notifImg,
-//   };
-//   new Notification(notifTitle, options);
-//   setTimeout(randomNotification, 3000);
-// }
+function randomNotification() {
+   const notifTitle = 'Title';
+   const notifBody = `Body `;
+   const notifImg = `./fprint.png`;
+   const options = {
+     body: notifBody,
+     icon: notifImg,
+   };
+   new Notification(notifTitle, options);
+    setTimeout(randomNotification, 3000);
+ }
